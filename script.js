fff0f2439cfc04f4dfbb6805a70e5e8951700153
Majed-Develops <majedbashir.dev@gmail.com>
2025-10-20 18:04:12 +0300

    dropdown

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ phoneBtn.addEventListener("click", () => {
     location.href = "tel: +962790699156";
 });
 
+document.querySelectorAll('.QA').forEach(qa => {
+  const answer = qa.querySelector('.answer');
 
+  qa.addEventListener('click', () => {
+    qa.classList.toggle('open');
+
+    if (qa.classList.contains('open')) {
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    } else {
+      answer.style.maxHeight = null;
+    }
+  });
+});
